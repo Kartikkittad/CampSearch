19d37bc185ef655ca06ef0ff661331dbf63a7cb2
Kartik Kittad <99243266+Kartik1824@users.noreply.github.com>
2023-03-25 13:28:44 +0530

    Add files via upload

---
+++ models/campground.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');                               //05
+const Schema = mongoose.Schema;
+
+const CampgroundSchema = new Schema({
+    title: String,
+    image: String,
+    price: Number,
+    description: String,
+    location: String
+});
+
+module.exports = mongoose.model('Campground', CampgroundSchema);
